ac418529fd64f8518a7106ede466e05f03b39204
Niek Raaijmakers <niekraaijmakers@gmail.com>
2020-08-21 16:24:37 +0200

    Put on working unit tests for carousel (#6)
    * Start with just 1 test
    
    * 1 more..
    
    * Another 1
    
    * Run number 4..
    
    Co-authored-by: Niek Raaijmakers <niek@adobe.com>

--- react-spa-components/src/container/carousel/v1/CarouselV1.test.tsx
+++ react-spa-components/src/container/carousel/v1/CarouselV1.test.tsx
@@ -75,7 +75,7 @@ const validateComponentPresent = (wrapper:ReactWrapper, text:string) =>{
     expect(dummyComp.text()).toEqual(text);
 };
 
-/*
+
 it('Renders a basic carousel properly and reacts on clicks', () => {
 
     // const Wrapped = withComponentMappingContext(AccordionV1);
@@ -102,8 +102,6 @@ it('Renders a basic carousel properly and reacts on clicks', () => {
     const nextButton = wrapper.find(".cmp-carousel__action--next");
     expect(nextButton).toHaveLength(1);
     nextButton.simulate('click');
-    wrapper.update();
-
 
     validateComponentPresent(wrapper, "Component2");
 
@@ -141,133 +139,133 @@ it('Changes when you switch slide in author mode', () => {
     validateComponentPresent(wrapper,"Component1");
 });
 
-
-it('Automatically slides forward', (done) => {
-
-    // const Wrapped = withComponentMappingContext(AccordionV1);
-    const wrapper = mount(<CarouselV1  {...defaultProps} componentMapping={ComponentMapping}/>);
-    const content = wrapper.find('.cmp-carousel__content');
-
-    expect(content).toHaveLength(1);
-    validateComponentPresent(wrapper, "Component1");
-
-    setTimeout(()=> {
-        wrapper.update();
-        validateComponentPresent(wrapper, "Component2");
-
-        done();
-    },1000);
-
-});
-
-it('Does NOT Automatically slide forward if we turn it off', (done) => {
-
-    // const Wrapped = withComponentMappingContext(AccordionV1);
-    const wrapper = mount(<CarouselV1  {...defaultProps} autoplay={false} componentMapping={ComponentMapping}/>);
-    const content = wrapper.find('.cmp-carousel__content');
-
-    expect(content).toHaveLength(1);
-
-    const pauseButton = wrapper.find(".cmp-carousel__action--pause");
-
-    expect(pauseButton).toHaveLength(0);
-
-    setTimeout(()=> {
-        validateComponentPresent(wrapper, "Component1");
-
-        done();
-    },1000);
-
-});
-
-it('Does NOT Automatically slide forward if we click pause, and resumes if we click resume', (done) => {
-
-    // const Wrapped = withComponentMappingContext(AccordionV1);
-    const wrapper = mount(<CarouselV1  {...defaultProps} componentMapping={ComponentMapping}/>);
-    const content = wrapper.find('.cmp-carousel__content');
-
-    expect(content).toHaveLength(1);
-
-    const pauseButton = wrapper.find(".cmp-carousel__action--pause");
-
-    pauseButton.simulate("click");
-
-    validateComponentPresent(wrapper, "Component1");
-
-    setTimeout(()=> {
-        wrapper.update();
-        validateComponentPresent(wrapper, "Component1");
-
-        const resumeButton = wrapper.find(".cmp-carousel__action--play");
-
-        resumeButton.simulate("click");
-        setTimeout( ()=> {
-            resumeButton.simulate("click");
-            done();
-        }, 500);
-
-
-    },500);
-
-});
-
-
-it('Temporary stops sliding if we hover over it, and resume once we hover out.', (done) => {
-
-    // const Wrapped = withComponentMappingContext(AccordionV1);
-    const wrapper = mount(<CarouselV1  {...defaultProps} componentMapping={ComponentMapping}/>);
-    const content = wrapper.find('.cmp-carousel__content');
-
-    expect(content).toHaveLength(1);
-
-    //trigger hover in
-    content.simulate('mouseenter');
-
-    setTimeout(()=> {
-        wrapper.update();
-        validateComponentPresent(wrapper, "Component1");
-
-
-        //trigger hover out
-        content.simulate('mouseleave');
-
-        setTimeout(() => {
-
-            validateComponentPresent(wrapper, "Component2");
-
-            done();
-        }, 1000);
-
-    },1000);
-
-});
-
-
-it('Does not temporarily stop sliding if I hover over it, if we disabled autopause', (done) => {
-
-    // const Wrapped = withComponentMappingContext(AccordionV1);
-    const wrapper = mount(<CarouselV1  {...defaultProps} autopauseDisabled={true}  componentMapping={ComponentMapping}/>);
-    const content = wrapper.find('.cmp-carousel__content');
-
-    expect(content).toHaveLength(1);
-
-    //trigger hover in
-    content.simulate('mouseenter');
-
-    validateComponentPresent(wrapper, "Component1");
-
-    setTimeout(()=> {
-        wrapper.update();
-        validateComponentPresent(wrapper, "Component2");
-
-
-        done();
-
-    },1000);
-
-});
-
-
+//fails in CI
+// it('Automatically slides forward', (done) => {
+//
+//     // const Wrapped = withComponentMappingContext(AccordionV1);
+//     const wrapper = mount(<CarouselV1  {...defaultProps} componentMapping={ComponentMapping}/>);
+//     const content = wrapper.find('.cmp-carousel__content');
+//
+//     expect(content).toHaveLength(1);
+//     validateComponentPresent(wrapper, "Component1");
+//
+//     setTimeout(()=> {
+//         wrapper.update();
+//         validateComponentPresent(wrapper, "Component2");
+//
+//         done();
+//     },1000);
+//
+// });
+// //
+// it('Does NOT Automatically slide forward if we turn it off', (done) => {
+//
+//     // const Wrapped = withComponentMappingContext(AccordionV1);
+//     const wrapper = mount(<CarouselV1  {...defaultProps} autoplay={false} componentMapping={ComponentMapping}/>);
+//     const content = wrapper.find('.cmp-carousel__content');
+//
+//     expect(content).toHaveLength(1);
+//
+//     const pauseButton = wrapper.find(".cmp-carousel__action--pause");
+//
+//     expect(pauseButton).toHaveLength(0);
+//
+//     setTimeout(()=> {
+//         validateComponentPresent(wrapper, "Component1");
+//
+//         done();
+//     },1000);
+//
+// });
+//
+// it('Does NOT Automatically slide forward if we click pause, and resumes if we click resume', (done) => {
+//
+//     // const Wrapped = withComponentMappingContext(AccordionV1);
+//     const wrapper = mount(<CarouselV1  {...defaultProps} componentMapping={ComponentMapping}/>);
+//     const content = wrapper.find('.cmp-carousel__content');
+//
+//     expect(content).toHaveLength(1);
+//
+//     const pauseButton = wrapper.find(".cmp-carousel__action--pause");
+//
+//     pauseButton.simulate("click");
+//
+//     validateComponentPresent(wrapper, "Component1");
+//
+//     setTimeout(()=> {
+//         wrapper.update();
+//         validateComponentPresent(wrapper, "Component1");
+//
+//         const resumeButton = wrapper.find(".cmp-carousel__action--play");
+//
+//         resumeButton.simulate("click");
+//         setTimeout( ()=> {
+//             resumeButton.simulate("click");
+//             done();
+//         }, 500);
+//
+//
+//     },500);
+//
+// });
+//
+//
+// it('Temporary stops sliding if we hover over it, and resume once we hover out.', (done) => {
+//
+//     // const Wrapped = withComponentMappingContext(AccordionV1);
+//     const wrapper = mount(<CarouselV1  {...defaultProps} componentMapping={ComponentMapping}/>);
+//     const content = wrapper.find('.cmp-carousel__content');
+//
+//     expect(content).toHaveLength(1);
+//
+//     //trigger hover in
+//     content.simulate('mouseenter');
+//
+//     setTimeout(()=> {
+//         wrapper.update();
+//         validateComponentPresent(wrapper, "Component1");
+//
+//
+//         //trigger hover out
+//         content.simulate('mouseleave');
+//
+//         setTimeout(() => {
+//
+//             validateComponentPresent(wrapper, "Component2");
+//
+//             done();
+//         }, 1000);
+//
+//     },1000);
+//
+// });
+//
+//
+// it('Does not temporarily stop sliding if I hover over it, if we disabled autopause', (done) => {
+//
+//     // const Wrapped = withComponentMappingContext(AccordionV1);
+//     const wrapper = mount(<CarouselV1  {...defaultProps} autopauseDisabled={true}  componentMapping={ComponentMapping}/>);
+//     const content = wrapper.find('.cmp-carousel__content');
+//
+//     expect(content).toHaveLength(1);
+//
+//     //trigger hover in
+//     content.simulate('mouseenter');
+//
+//     validateComponentPresent(wrapper, "Component1");
+//
+//     setTimeout(()=> {
+//         wrapper.update();
+//         validateComponentPresent(wrapper, "Component2");
+//
+//
+//         done();
+//
+//     },1000);
+//
+// });
+//
+//
 it('Renders a basic carousel without autoplay', () => {
 
     // const Wrapped = withComponentMappingContext(AccordionV1);
@@ -300,4 +298,3 @@ it('Renders out all slides in author mode with hidden CSS', () => {
     expect(dummyComp).toHaveLength(2);
 
 });
-*/
