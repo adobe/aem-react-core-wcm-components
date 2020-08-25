9f3386f1a3feccb7b15b62b2660875e0a6d204f1
Niek Raaijmakers <niek@adobe.com>
2020-08-25 10:14:32 +0200

    Separated the aliases into 1 central file.

--- examples/react-spacomponents-example-project/craco.config.js
+++ examples/react-spacomponents-example-project/craco.config.js
@@ -1,5 +1,5 @@
 const CracoAlias = require("craco-alias");
-
+const aliases = require("./config/aliases");
 module.exports = {
     plugins: [
         {
@@ -7,17 +7,7 @@ module.exports = {
             options: {
                 source: "options",
                 baseUrl: "./",
-                aliases: {
-                    "react": "./node_modules/react",
-                    "react-router": "./node_modules/react-router",
-                    "react-router-dom": "./node_modules/react-router-dom",
-                    "@adobe/cq-react-editable-components":  "./node_modules/@adobe/cq-react-editable-components",
-                    "@adobe/cq-spa-component-mapping":  "./node_modules/@adobe/cq-spa-component-mapping",
-                    "@adobe/cq-spa-page-model-manager":  "./node_modules/@adobe/cq-spa-page-model-manager",
-                    "@adobe/aem-core-components-react-base":  "./node_modules/@adobe/aem-core-components-react-base",
-                    "@adobe/aem-core-components-react-spa":  "./node_modules/@adobe/aem-core-components-react-spa"
-
-                }
+                aliases: aliases
             }
         }
     ]
